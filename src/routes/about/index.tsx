4d666d768ef2fe08b8609c91825cb055ff9fd14d
WeiJian123-tech <zhen16999@proton.me>
2025-09-02 17:31:35 -0400

    Began programming about page and filling in content for it. Created hero section for about page which includes a title, text, and a selfie of myself. Changed assets directory images from jpg to compressed png. Swapped heroimage.module.css background image to the compressed png background.

--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -2,6 +2,7 @@ import { component$ } from "@builder.io/qwik";
 import { type DocumentHead } from "@builder.io/qwik-city";
 import { Navbar } from "~/components/navbar/navbar";
 import { Redirlink } from "~/components/Redirlink";
+import Image from '~/assets/wjz_portrait-min.png?jsx';
 
 export default component$(() => {
     interface footerData {
@@ -17,10 +18,21 @@ export default component$(() => {
     ];
     
     return (
-        <>
+        <div class="flex flex-col items-center justify-center w-screen min-h-screen">
             <Navbar />
-            <main class="flex flex-col items-center justify-center-safe w-full h-screen">
-                <h1>About</h1>
+            <main class="flex flex-col flex-grow items-center justify-center-safe w-full overflow-y-auto">
+                <div class="flex flex-row items-start justify-center w-auto h-screen bg-[#90b5cc]">
+                    <div class="flex flex-col items-start justify-start p-2 w-1/2 h-full">
+                        <h1 class="text-3xl font-serif font-bold">Hello! 👋 My name is Wei Jian Zhen.</h1>
+                        <p class="font-serif font-normal">
+                            I'm a computer science graduate, web developer, and programmer who has studied various programming languages like React.js and Java. I usually create open source GitHub projects on my own free time. 🙂
+                        </p>
+                    </div>
+
+                    <div class="p-2 w-1/2 h-full">
+                        <Image />
+                    </div>
+                </div>
             </main>
 
             <footer class="flex flex-col items-center justify-evenly w-full h-auto text-center">
@@ -33,7 +45,7 @@ export default component$(() => {
                 />
                 ))}
             </footer>
-        </>
+        </div>
     );
 });
 
