ed2444c1ea596375b198693ebdd78de13df38252
WeiJian123-tech <zhen16999@proton.me>
2025-09-05 11:37:31 -0400

    Refactored navbar component code. Created a mobile hamburger dropdown menu when the viewport width is under 1024px which covers the screen with four navigation links aligned vertically. If the user's viewport is 1024px or above, the user will see the old navigation menu which is a horizontal menu with four links that now has bigger width.

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,7 +1,9 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal } from "@builder.io/qwik";
 import { Link } from "@builder.io/qwik-city";
 
 export const Navbar = component$(() => {
+
+    const isHamClicked = useSignal(false);
 
     return (
         <nav class="flex flex-row items-center justify-between m-0 p-0 w-full h-[6rem] sticky top-0 z-10 bg-slate-200">
@@ -16,13 +18,37 @@ export const Navbar = component$(() => {
                     />
                 </a>
             </div>
-            
-            <div class="flex flex-row items-center justify-around w-auto h-full bg-slate-200">
+
+            <div class="w-auto h-full">
+                <button 
+                onClick$={() => isHamClicked.value = !isHamClicked.value} 
+                aria-label="Toggle Dropdown Menu" 
+                class="block w-auto h-full text-6xl border-l-2 border-slate-950 lg:hidden"
+                >
+                    ☰
+                </button>
+            </div>
+
+            <div class={`absolute top-0 right-0 ${isHamClicked.value? 'flex flex-col' : 'hidden'} w-full h-screen z-50 bg-slate-300 lg:static lg:flex lg:flex-row lg:w-1/2 lg:h-full xl:w-1/4`}>
+                <button 
+                onClick$={
+                    () => {
+                        isHamClicked.value = !isHamClicked.value;
+                    }
+                } 
+                aria-label="Close Dropdown Menu" 
+                class="self-end w-[6rem] h-auto text-4xl text-red-500 bg-slate-100 lg:hidden"
+                >
+                    X
+                </button>
+                
                 <Link href="/" class="flex grow items-center justify-center w-full h-full font-sans font-bold px-1 border-1 border-black hover:bg-slate-300 active:bg-slate-300">Home</Link>
                 <Link href="/about" class="flex grow items-center justify-center w-full h-full font-sans font-bold px-1 border-1 border-black hover:bg-slate-300 active:bg-slate-300">About</Link>
                 <Link href="/projects" class="flex grow items-center justify-center w-full h-full font-sans font-bold px-1 border-1 border-black hover:bg-slate-300 active:bg-slate-300">Projects</Link>
                 <Link href="/contact" class="flex grow items-center justify-center w-full h-full font-sans font-bold px-1 border-1 border-black hover:bg-slate-300 active:bg-slate-300">Contact</Link>
             </div>
+
+            
         </nav>
     );
 });
