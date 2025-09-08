c13bfcedfaecfcc0ea76c6b051680f1b368d0f9f
WeiJian123-tech <zhen16999@proton.me>
2025-09-08 17:59:24 -0400

    Increased the font size of the text in the about page. Centered text and heading. Created paragraphs in the Hero section of the about page. Added alt text to the images of the about page. Minor changes and fixes.

--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -17,20 +17,21 @@ export default component$(() => {
 
                 <div class="flex flex-col w-full h-auto bg-[#c0e0f3]">
 
-                    <div class="flex items-start justify-center max-h-1/4 p-1 overflow-hidden border-1 border-black">
-                        <h1 class="text-3xl font-serif font-bold">Hello! 👋 My name is Wei Jian Zhen.</h1>
+                    <div class="flex flex-col items-center justify-center max-h-1/4 text-center overflow-hidden">
+                        <h1 class="text-4xl font-serif font-bold">Hello! 👋</h1>
+                        <h1 class="text-4xl font-serif font-bold">My name is Wei Jian Zhen.</h1>
                     </div>
 
-                    <div class="overflow-hidden border-1 border-red-500">
+                    <div class="flex flex-row items-center justify-center my-2 overflow-hidden border-4 border-solid border-slate-900">
                         <Selfie alt="A selfie portrait of Wei Jian Zhen" />
                     </div>
 
-                    <div class="p-1 overflow-hidden border-1 border-green-500">
-                        <p class="font-serif font-normal">
+                    <div class="px-2 py-1 overflow-hidden">
+                        <p class="py-2 text-2xl font-serif font-normal indent-8">
                             I'm a web developer and programmer who has studied various programming languages like React.js and Java. 
                             I usually create open source GitHub projects on my own free time prioritizing user interface experience and quirky ideas. 😊
                         </p>
-                        <p class="font-serif font-normal">
+                        <p class="py-2 text-2xl font-serif font-normal indent-8">
                             During my free time, I like to play video games like <Redirlink 
                             pathName={"https://www.konami.com/yugioh/masterduel/us/en/"} 
                             label={"Yugioh Master Duel"} 
@@ -39,26 +40,27 @@ export default component$(() => {
                             label={"Super Smash Bros."} />. 🎮
                             I like to play video games so much that I wanted to make a video game myself. 
                         </p>
-                        <p class="font-serif font-normal">
+                        <p class="py-2 text-2xl font-serif font-normal indent-8">
                             Therefore during my middle school years,
                             I decided with my stary-eyed passion to start learning how to program from <Redirlink pathName={"https://www.khanacademy.org/computing/computer-programming/programming"} label={"Khan Academy"} />. 🤩
-                            Programming was intuitive yet boring and convoluted 
-                            because I had to figure out which steps to place in order for ther program to work 
-                            however I need to determine how it should be ordered along with syntax, bugs, etc. 👾
+                            Programming was intuitive because I just had to figure out which steps to place in order for ther program to work.
+                            Yet programming was boring and convoluted since I had to determine exactly <i>how</i> it should be ordered along with syntax, bugs, etc. 👾
                         </p>
-                        <p class="font-serif font-normal">
+                        <p class="py-2 text-2xl font-serif font-normal indent-8">
                             You would think a program would be able to run even without a missing semicolon, huh? 🙄
                             Wait what? How was I supposed to know I would inevitably create an infinite loop in my JavaScript program!? 😖
+                            <br />
+                            <br />
                             Oh... Missing ending condition. 😑 😐
                         </p>
-                        <p class="font-serif font-normal">
+                        <p class="py-2 text-2xl font-serif font-normal indent-8">
                             Anyways, overtime I researched and failed faster to improve my programming skills and attain greater knowledge of computer science as a whole.
                             Every glitch/bug was an utter annoyance and an oppurtunity to learn how to fix it. 
                             Every all-nighter was tiresome and built character along the way.
                             And all the people I met and worked with helped guide my path along my ever continuous programming and professional development. 💯
                             When the problems got difficult, I asked my teachers/professors for help or surfed the web for any solutions.
                         </p>
-                        <p class="font-serif font-normal">
+                        <p class="py-2 text-2xl font-serif font-normal indent-8">
                             Programs are meant to be solutions to problem(s). 
                             How will this life pave a professional programming career for me and for maybe you, dear visitor? 🤔
                             Please contact me through my social in the footer down below to answer that question. 😉
@@ -67,33 +69,12 @@ export default component$(() => {
 
                 </div>
 
-                {/*
-                <div class="flex flex-row items-start justify-center w-full h-full bg-[#90b5cc]">
-                    <div class="flex flex-col items-start justify-start p-2 w-1/2 h-full">
-                        <h1 class="text-3xl font-serif font-bold">Hello! 👋 My name is Wei Jian Zhen.</h1>
-                        <p class="font-serif font-normal">
-                            I'm a computer science graduate, web developer, and programmer who has studied various programming languages like React.js and Java. 
-                            I usually create open source GitHub projects on my own free time. 🙂
-                        </p>
-
-                        <p class="mt-3 font-serif font-normal">
-                            For more information, scroll or swipe down below! ⬇️
-                        </p>
-                    </div>
-
-                    <div class="p-2 w-1/2 h-full">
-                        <Selfie />
-                    </div>
-                </div>
-                */}
-                
-
                 <div class="flex flex-row items-start justify-center w-full h-full bg-orange-300">
-                    <div class="flex flex-col items-start justify-start p-2">
-                        <h2 class="text-xl font-serif font-bold">
+                    <div class="flex flex-col items-center justify-start p-2">
+                        <h2 class="text-3xl font-serif font-bold text-center">
                             Professional Background
                         </h2>
-                        <p class="font-serif font-normal">
+                        <p class="py-2 indent-8 text-2xl font-serif font-normal">
                             I have an undergraduate computer science degree from CUNY Brooklyn College. 
                             I learned about data structures, discrete structures, C++ programming paradigms, and more.
                             I attended a competitive web development fellowship called CUNY Tech Prep. 
@@ -104,10 +85,10 @@ export default component$(() => {
                             my <Redirlink pathName={"../projects"} label={"projects"} /> section to explore the wonderful projects I created over the years. 😄
                         </p>
 
-                        <h3 class="text-lg font-serif font-bold">
+                        <h3 class="text-2xl font-serif font-bold text-center">
                             My Top 5 Programming Languages
                         </h3>
-                        <ol class="pl-10 list-decimal font-mono">
+                        <ol class="text-xl py-4 list-decimal font-mono">
                             <li>Java</li>
                             <li>JavaScript</li>
                             <li>Qwik.js</li>
@@ -118,28 +99,28 @@ export default component$(() => {
                 </div>
 
                 <div class="flex flex-row items-start justify-center w-full h-full bg-green-300">
-                    <div class="flex flex-col items-start justify-start p-2">
-                        <h2 class="text-xl font-serif font-bold">
+                    <div class="flex flex-col items-center justify-center p-2">
+                        <h2 class="text-3xl font-serif font-bold text-center">
                             Mission Statement
                         </h2>
-                        <p class="font-serif font-normal">
+                        <p class="p-2 text-2xl font-serif font-normal">
                             To continuously educate, adapt, and innovate in an ever-changing tech field providing quality user experiences.
                         </p>
                     </div>
                 </div>
 
-                <div class="flex flex-row items-start justify-center w-full h-full bg-violet-300">
+                <div class="flex flex-row items-start justify-center w-full h-full bg-[#ceabfc]">
                     <div class="flex flex-col items-center justify-start p-2">
-                        <h2 class="text-xl font-serif font-bold text-center">
+                        <h2 class="text-3xl font-serif font-bold text-center">
                             Featured Project!
                         </h2>
                         
                         <div class="flex flex-col items-center justify-center pt-4 text-center">
                             <Link href="https://github.com/WeiJian123-tech/Birdlogger.git">
-                                <BirdloggerImg />
+                                <BirdloggerImg alt="A screenshot of the GitHub repository webpage for Birdlogger." />
                             </Link>
 
-                            <Redirlink pathName={"https://github.com/WeiJian123-tech/Birdlogger.git"} label={"Birdlogger"} class={"text-xl"} />
+                            <Redirlink pathName={"https://github.com/WeiJian123-tech/Birdlogger.git"} label={"⭐ Birdlogger ⭐"} class={"my-4 text-2xl bg-slate-100/50"} /> 
                         </div>
                     </div>
                 </div>
