335e38b0994781d64edf74572829bd409e23f413
WeiJian123-tech <zhen16999@proton.me>
2025-09-09 15:08:42 -0400

    Social buttons get closer to each other when the viewport width is 1024px or above. Created a small copyright notice at the bottom of the footer.

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -18,31 +18,38 @@ export const Footer = component$(() => {
             url: 'mailto:zhen16999@proton.me', 
             imagePath: protonmailIcon, 
             altText: 'Proton Mail icon that redirects to my email address', 
+            class: 'mx-6'
         },
         {
             url: 'https://www.linkedin.com/in/wei-jian-zhen-la', 
             imagePath: linkedinIcon, 
             altText: 'LinkedIn icon that redirects to my LinkedIn profile',
+            class: 'mx-6'
         },
         {
             url: 'https://github.com/WeiJian123-tech', 
             imagePath: githubIcon, 
             altText: 'GitHub icon that redirects to my LinkedIn profile', 
+            class: 'mx-6'
         },
     ];
 
     return (
-        <footer class="flex flex-row items-center justify-evenly p-2 w-full h-auto border-t-1 border-slate-900 bg-[#668ff4] text-center">
+        <footer class="flex flex-col items-center justify-between p-2 w-full h-auto border-t-1 border-slate-900 bg-[#668ff4] text-center lg:justify-center">
 
-            {socialBtnAttr.map((attr) => (
-                <Socialbtn 
-                    key={attr.url}
-                    url={attr.url}
-                    imagePath={attr.imagePath}
-                    altText={attr.altText}
-                    class={attr.class}
-                />
-            ))}
+            <div class="flex flex-row items-center justify-evenly p-2 w-full h-auto lg:justify-center">
+                {socialBtnAttr.map((attr) => (
+                    <Socialbtn 
+                        key={attr.url}
+                        url={attr.url}
+                        imagePath={attr.imagePath}
+                        altText={attr.altText}
+                        class={attr.class}
+                    />
+                ))}
+            </div>
+
+            <small class="text-gray-800">© Wei Jian Zhen All Rights Reserved 2025</small>
             
             
         </footer>
