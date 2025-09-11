0b26112eb23af1057306d28f81597feda30e68b7
WeiJian123-tech <zhen16999@proton.me>
2025-09-11 15:37:05 -0400

    Testing

--- vite.config.ts
+++ vite.config.ts
@@ -34,7 +34,7 @@ export default defineConfig(({ command, mode }): UserConfig => {
       exclude: [],
     },
     build: {
-      outDir: '../dist', // Adjust this to your desired output directory
+      outDir: 'dist', // Adjust this to your desired output directory
     },
     /**
      * This is an advanced setting. It improves the bundling of your server code. To use it, make sure you understand when your consumed packages are dependencies or dev dependencies. (otherwise things will break in production)
